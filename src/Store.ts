c7a73d817db5d16dddd519415804641d777632f5
Your Name <you@example.com>
2022-04-01 01:31:25 +0400

    Without console.logs

--- src/Store.ts
+++ src/Store.ts
@@ -27,7 +27,6 @@ class Store {
     this.brand = brand;
     this.product = product;
     let res: any;
-    console.log("fetching");
     try {
       if (this.brand !== "" && this.product !== "") {
         res = await axios.get(
@@ -38,7 +37,6 @@ class Store {
       } else if (this.product !== "" && this.brand === "") {
         res = await axios.get(baseURL + `?product_type=${product}`);
       }
-      console.log(res);
       if (res.data.length > 0) {
         let arr: Card[] = [];
         res.data.map((card: Card) => {
@@ -60,13 +58,11 @@ class Store {
     }
   }
   toLike(id: string) {
-    console.log(this.likedCards);
     this.cards = this.cards.map((card) =>
       card.id === id ? { ...card, isLiked: true } : card
     );
     const likedCard = this.cards.filter((card) => card.id === id);
     this.likedCards.push(likedCard[0]);
-    console.log(this.likedCards);
   }
 
   toUnlike(id: string) {
@@ -74,7 +70,6 @@ class Store {
       card.id === id ? { ...card, isLiked: false } : card
     );
     this.likedCards = this.likedCards.filter((card) => card.id !== id);
-    console.log(this.likedCards);
   }
 
   removeCard(id: string) {

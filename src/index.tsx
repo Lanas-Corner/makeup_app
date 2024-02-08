61ac69508d44009cadf9ca84c9b958a0e51e92eb
apsu96 <apsu0161@gmail.com>
2024-02-08 15:29:17 +0500

    chore: update antd package, add config provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { ConfigProvider } from "antd";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider>
+      <App />
+    </ConfigProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
